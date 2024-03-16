22ddad473a453b4c3c65f54b4eb4256013dc87b8
nikoloztepnadze25 <nikoloztepnadze25@gmail.com>
2024-03-16 19:42:42 +0400

    second commit

---
+++ day 01/day1.py
@@ -0,0 +1,43 @@
+from turtle import*
+
+
+width(7)
+color("purple")
+forward(200)
+left(90)
+
+#we want paint a house
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(60)
+left(90)
+
+color("yellow")
+forward(120)
+right(90)
+
+forward(50)
+right(90)\
+
+forward(120)
+
+penup()
+goto(200, 200)
+pendown()
+
+color("red")
+right(150)
+
+forward(200)
+left(120)
+forward(200)
+
+exitonclick()
