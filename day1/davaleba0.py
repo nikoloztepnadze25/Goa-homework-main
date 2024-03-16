7138a874e34f19cf39eb3df8cda09fc07dcf4949
nikoloztepnadze25 <nikoloztepnadze25@gmail.com>
2024-03-16 18:26:16 +0400

    first commit

---
+++ day1/davaleba0.py
@@ -0,0 +1,41 @@
+from turtle import*
+
+width(7)
+color("purple")
+forward(200)
+
+#drawing a house
+
+left(90)
+forward(200)
+
+left(90)
+forward(200)
+
+left(90)
+forward(200)
+
+
+#and of drawing
+
+#drawing a door
+
+left(90)
+forward(60)
+color("yellow")
+left(90)
+forward(100)
+
+right(90)
+forward(50)
+
+right(90)
+forward(100)
+
+penup()
+
+goto(200, 200)
+
+forward(28)
+color("red")
+exitonclick()
---
+++ day1/lesson1.py
@@ -0,0 +1,40 @@
+from turtle import*
+
+
+print("nikoloz tepnaze")
+print(10)
+
+
+#print("hello")
+#print(10)
+
+
+#დაპრინტეთ თქვენი სახელი და გვარი
+#დაპრინტეთ თქვენი ასაკი
+
+
+
+circle(50)
+
+color(50)
+
+
+name="gabrieli"
+
+print(name)
+
+item="bike"
+
+
+#item არის ცვლადი სახელი
+#bike არის ცვლადი მნიშნელობა
+
+print(10)
+
+#virible name - ცვლადის სახელი
+#variable value - ცვლადის მნიშნელობა
+ #verables-ცვლადი
+
+
+
+exitonclick()
